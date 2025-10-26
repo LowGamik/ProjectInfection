8b9b4e8a1da514faa712b01c03f92586c026b69c
LowGamik <lowgamik@gmail.com>
2025-10-26 14:04:55 +0100

    addedd typewriter, visitor class, and clearing screen for diffrent environments

--- main.cpp
+++ main.cpp
@@ -1,14 +1,25 @@
+#ifdef _WIN32
+    #define CLEAR_SCREEN() system("cls")
+#else
+    #define CLEAR_SCREEN() system("clear")
+#endif
+
 #include <iostream>
 #include <string>
+#include <vector>
+#include <thread>
+#include <chrono>
 using namespace std;
+using namespace this_thread;
 
 class Guard{
     private:
         string name;
-        int sanity;
+        int sanity = 80;
     public:
-        void setName(string inputName){
-            name = inputName;
+        void setName(){
+            cout << "What is your characters name?" <<endl;
+            getline(cin, name);
         }
         string getName(){
             return name;
@@ -21,13 +32,39 @@ class Guard{
         }
 };
 
+
+
+class Visitor{
+    private:
+        string name;
+        bool isInfected;
+        bool isVacinated;
+    public:
+
+};
+
+void typeWriter(const string& sentence, int timeMillisecconds);
+
 int main(){
     Guard player;
-    string name;
+    vector<Visitor> visitors;
+    visitors.resize(3);
+
+    CLEAR_SCREEN();
+
     cout << "hello, world!" << endl;
-    getline(cin, name);
-    cout << name << endl;
-    player.setName(name);
-    cout << "Guard's name is " << player.getName() << endl;
+    player.setName();
+
+    CLEAR_SCREEN();
+
+    typeWriter("Guard's name is " + player.getName(), 100);
     return 0;
+}
+
+void typeWriter(const string& sentence, int timeMillisecconds) {
+    for (char c : sentence) {
+        cout << c << flush;
+        sleep_for(chrono::milliseconds(timeMillisecconds));
+    }
+    cout << endl;
 }
