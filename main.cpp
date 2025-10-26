28a483a8ca6135484c628648c2c84e92e8e65d7b
LowGamik <lowgamik@gmail.com>
2025-10-26 13:15:51 +0100

    modified gitignore and added guard class

--- main.cpp
+++ main.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-class Player{
+class Guard{
     private:
         string name;
         int sanity;
+    public:
+        void setName(string inputName){
+            name = inputName;
+        }
+        string getName(){
+            return name;
+        }
+        void setSanity(int inputSanity){
+            sanity = inputSanity;
+        }
+        int getSanity(){
+            return sanity;
+        }
 };
 
 int main(){
+    Guard player;
+    string name;
     cout << "hello, world!" << endl;
+    getline(cin, name);
+    cout << name << endl;
+    player.setName(name);
+    cout << "Guard's name is " << player.getName() << endl;
     return 0;
 }
