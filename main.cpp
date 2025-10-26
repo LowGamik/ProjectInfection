c60ea90f896cfafd30d5dbb666fce127b6fb9d20
LowGamik <lowgamik@gmail.com>
2025-10-26 12:47:52 +0100

    Added gitignore and main file

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class Player{
+    private:
+        string name;
+        int sanity;
+};
+
+int main(){
+    cout << "hello, world!" << endl;
+    return 0;
+}
